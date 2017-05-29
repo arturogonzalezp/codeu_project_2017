a8ed2f00462e5c7f0920bcda9100df279bced29c
raguirre <raguirre00@gmail.com>
2017-05-28 21:58:26 -0700

    Processes file messages

--- src/codeu/chat/server/Server.java
+++ src/codeu/chat/server/Server.java
@@ -124,16 +124,17 @@ public final class Server {
       final Uuid conversation = Uuid.SERIALIZER.read(in);
       final String content = Serializers.STRING.read(in);
 
+      final Message message;
+
       // In a NEW_FILE_MESSAGE_REQUEST the content variable will hold the filename, including extension.
       if(type == NetworkCode.NEW_FILE_MESSAGE_REQUEST) {
 
         final byte[] file = Serializers.BYTES.read(in);
+        message = controller.newMessage(author, conversation, content, file);
 
-        // At this point we have the file on the server as an array of bytes.
-        // We can do whatever we need with this here.
+      } else {
+        message = controller.newMessage(author, conversation, content);
       }
-
-      final Message message = controller.newMessage(author, conversation, content);
 
       Serializers.INTEGER.write(out, type == NetworkCode.NEW_MESSAGE_REQUEST ?
               NetworkCode.NEW_MESSAGE_RESPONSE : NetworkCode.NEW_FILE_MESSAGE_RESPONSE);
